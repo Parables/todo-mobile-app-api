1b3dd2aba91c82549f859e057e60a691410689bd
Parables <parables95@gmail.com>
2021-12-01 11:05:58 +0000

    inital  commit ... created a server, added routes, and connected to mongoDB

---
+++ index.js
@@ -0,0 +1,53 @@
+// import the express package
+import express from 'express';
+//import the mongoose package
+import mongoose from 'mongoose';
+//import the dotenv package
+import dotenv from 'dotenv';
+// configure dotenv
+dotenv.config();
+// process.env.ENV_NAME
+
+
+
+// create an instance of express
+const app = express();
+
+
+// create a variable for our PORT number
+const PORT = 5000;
+
+
+// create routes to make request to the serve
+app.get('/', (req, res)=>{
+    res.send('Hello Worlds');
+});
+
+app.post('/', (req, res)=>{
+    res.send('Hello Worlds');
+});
+
+app.put('/', (req, res)=>{
+    res.send('Hello Worlds');
+});
+
+app.patch('/', (req, res)=>{
+    res.send('Hello Worlds');
+});
+app.delete('/', (req, res)=>{
+    res.send('Hello Worlds');
+});
+
+
+// connect to MongoDBAtlas
+mongoose.connect(process.env.MONGO_DB_CON_sTRING, (error)=>{
+    if(error){
+     return   console.log(`Failed to connect to MongDB ${error}`)
+    }else{
+        console.log('MongoDB is connected')
+        // start the server to listen to incoming request
+// on the specified PORT
+app.listen(PORT, ()=>console.log(`Server is up and running 🚀🚀🚀 on PORT: ${PORT}`));
+    }
+})
+
