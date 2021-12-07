ee631fc51ed653efb21df702d0ccb791b8a56b32
Parables <parables95@gmail.com>
2021-12-07 12:10:17 +0000

    last checkpoint

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 // import the express package
 import express from "express";
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import {Todo} from './models/todo.js'
+
+dotenv.config();
 
 // create an instance of express
 const app = express();
 
+app.use(express.json());
+
+
+
 // create a variable for our PORT number
-const PORT = process.env.PORT ?? 5000;
+const PORT = process.env.PORT ?? 5432;
 
 // create routes to make request to the serve
-app.get("/", async (req, res) => {
-  res.send("Hello World");
-});
+app.get("/", async (req, res) => res.send("Hello World"));
 
 // create routes to make request to the serve
 app.get("/todos", async (req, res) => {
@@ -61,15 +68,21 @@ app.delete("/todo/:todoID", async (req, res) => {
   //find and update a model by
   // passing in the id and a callback function
   // that takes in the error and the deletedDocument
-  Todo.findByIdAndDelete(req.params.todoID)
-    .then(() => {
-      // console.log("Deleted ", deletedTodo);
+  try {
+    await  Todo.findByIdAndDelete(req.params.todoID, (error, doc)=>{
+if(!error)
       res.send(`Todo with _id: ${req.params.todoID} has been deleted`);
+    })
+  } catch (error) {
+    console.log("Error: ", error);
+  }
+   /*  .then(() => {
+      // console.log("Deleted ", deletedTodo);
     })
     .catch((error) => {
       console.log(`Failed to delete todo: ${error}`);
       res.send(`Failed to delete todo: ${error}`);
-    });
+    }); */
 });
 
 // connect to MongoDBAtlas
@@ -81,7 +94,7 @@ mongoose.connect(process.env.MONGO_DB_CON_STRING, (error) => {
     // start the server to listen to incoming request
     // on the specified PORT
     app.listen(PORT, () =>
-      console.log(`Server is up and running 🚀🚀🚀 on PORT: ${PORT}`)
-    );
+     { console.log(`Server is up and running 🚀🚀🚀 on PORT: ${PORT}`)
+   } );
   }
 });
